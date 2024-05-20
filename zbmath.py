ee7a110aa67590b8dca6f6e501c12531037c6e87
FMuro <fmuro@us.es>
2024-05-20 22:00:37 +0200

    download json from zbmath api

--- zbmath.py
+++ zbmath.py
@@ -1,11 +1,12 @@
 import urllib
 import urllib.request
-import xmltodict
 import json
-url = 'https://api.zbmath.org/v1/document/_structured_search?page=0&results_per_page=100&author%20code=muro.fernando'
-data = urllib.request.urlopen(url)
-dict = json.loads(data.read().decode('utf-8'))
-# dict = xmltodict.parse(data.read().decode('utf-8'))
-# print(data.read().decode('utf-8'))
-for item in dict['result']:
-    print(item['title']['title'])
+authors = ['muro.fernando', 'gonzalez-meneses.juan', 'flores.ramon-j', 'silvero.marithania', 'carmona-sanchez.v', 'cumplido.maria', 'manchon.pedro-m-g']
+baseurl = 'https://api.zbmath.org/v1/document/_structured_search?page=0&results_per_page=100&author%20code='
+f = open("zbmath.json", "w")
+for author in authors:
+    data = urllib.request.urlopen(
+        baseurl+author).read().decode('utf-8')
+    f.write(data)
+    f.write('\n\n')
+f.close()
