be5b8d9ed87757a3bf4d0c7c8c27a9aab7377efb
FMuro <fmuro@us.es>
2024-05-23 17:53:15 +0200

    2 functions: get bibtex from zbmath and bibtex to CSL JSON

--- bibliographer.py
+++ bibliographer.py
@@ -1,19 +1,20 @@
-import urllib
 import urllib.request
+import tempfile
+import subprocess
 
-zbmath_author_IDs = '{"IDs":['muro.fernando', 'gonzalez-meneses.juan', 'flores.ramon-j', 'silvero.marithania', 'carmona-sanchez.v', 'cumplido.maria', 'manchon.pedro-m-g']}'
+zbmath_author_IDs = "{'IDs':['muro.fernando', 'gonzalez-meneses.juan', 'flores.ramon-j', 'silvero.marithania', 'carmona-sanchez.v', 'cumplido.maria', 'manchon.pedro-m-g']}"
 zbmath_bibtex_baseurl = 'https://zbmath.org/bibtexoutput/?q=ia%3A'
 zbmath_api_baseurl = 'https://api.zbmath.org/v1/document/_structured_search?author%20code='
 
-def get_bibtex():
-    f = open("zbmath.bib", "w")
-    f.write('{')
-    for author in zbmath_author_IDs['IDs']:
-        f.write('"'+author+'":')
-        # the replacement {\'{\i}} -> í is done in order to avoid biblatex/biber errors
-        data = urllib.request.urlopen(
-            zbmath_bibtex_baseurl+author).read().decode('utf-8').replace("{\\'{\i}}", "í")
-        f.write(data)
-        f.write('\n\n')
-        f.write('{')
-    f.close()
+def get_bibtex_from_zbmath(zbmath_author_ID):
+    # the replacement {\'{\i}} -> í is done in order to avoid biblatex/biber errors
+    return urllib.request.urlopen(
+            zbmath_bibtex_baseurl+zbmath_author_ID).read().decode('utf-8').replace("{\\'{\\i}}", "í")
+
+def bibtex_to_csljson(bibtex):
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(bibtex)
+        f.close()
+        return subprocess.check_output(['pandoc', '-f', 'bibtex', f.name, '-t', 'csljson']).decode('utf-8')
+
+print(bibtex_to_csljson(get_bibtex_from_zbmath('muro.fernando')))
