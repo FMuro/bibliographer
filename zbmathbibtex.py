aeed2894b78997d2d67e94e692a5b11d3277a667
FMuro <fmuro@us.es>
2024-06-06 13:54:55 +0200

    small stuff

--- zbmathbibtex.py
+++ zbmathbibtex.py
@@ -1,12 +1,13 @@
+#!./bin/python3
 import urllib
 import urllib.request
 baseurl = 'https://zbmath.org/bibtexoutput/?q=ia%3A'
-authors = ['muro.fernando', 'gonzalez-meneses.juan', 'flores.ramon-j', 'silvero.marithania', 'carmona-sanchez.v', 'cumplido.maria', 'manchon.pedro-m-g']
+authors = ['muro.fernando']
 f = open("zbmath.bib", "w")
 for author in authors:
     # the replacement {\'{\i}} -> í is done in order to avoid biblatex/biber errors
     data = urllib.request.urlopen(
-        baseurl+author).read().decode('utf-8').replace("{\\'{\i}}", "í")
+        baseurl+author).read().decode('utf-8')
     f.write(data)
     f.write('\n\n')
 f.close()
