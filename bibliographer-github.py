66a2c24f7e01e9b1c1fa567c57fb4b0bacdd402b
FMuro <FMuro@users.noreply.github.com>
2024-06-13 17:32:02 +0200

    minor fixes to github action

--- bibliographer-github.py
+++ bibliographer-github.py
@@ -1,6 +1,5 @@
 #!./bin/python3
 from libbibliographer import get_bibtex_from_zbmath
 
-data = merged_data_dict('muro.fernando','0000-0001-8457-9889')
 with open('output.bib', 'w') as outfile:
     outfile.write(get_bibtex_from_zbmath('muro.fernando'))
