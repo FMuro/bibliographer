29ad9e79ab68cea1ee0a278443fae688bc43470c
FMuro <fmuro@us.es>
2025-06-02 08:20:58 +0200

    fix script

--- bibliographer.py
+++ bibliographer.py
@@ -1,7 +1,9 @@
-#!./bin/python3
+#!.venv/bin/python3
 import libbibliographer
+import yaml
+import json
 
-data = merged_data_dict('muro.fernando','0000-0001-8457-9889')
+data = libbibliographer.merged_data_dict('muro.fernando','0000-0001-8457-9889')
 with open('output.yml', 'w') as outfile:
     yaml.dump(data, outfile, default_flow_style=False)
 with open("output.json", "w") as outfile: 
