fcf8e5157db30b0b3daa05a2a03d14dadb8d8ac3
Fernando Muro <fmuro@us.es>
2026-09-18 22:49:11 +0200

    Missing : in if

--- openalex.py
+++ openalex.py
@@ -62,7 +62,7 @@ def get_source_display_name_from_openalex(openalex_json: dict[str, Any], doi: st
 
     for item in openalex_json.get("results", []):
         if item.get("ids", {}).get("doi") == doi_url:
-            if item.get("primary_location", {}).get("source", {}).get("display_name") != "repository"
+            if item.get("primary_location", {}).get("source", {}).get("display_name") != "repository":
                 return item.get("primary_location", {}).get("source", {}).get("display_name")
 
     return None
